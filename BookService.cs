4ab20438cf6780b1803cdcad2d8415ce15104b9d
GRA8EY <omarwaleedsaad@gmail.com>
2024-09-24 21:49:22 +0300

    Add files via upload

---
+++ BookService.cs
@@ -0,0 +1,55 @@
+﻿using LibraryAPI.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryAPI.Services
+{
+    public class BookService
+    {
+        private static List<Book> Books = new List<Book>();
+
+        // Get all books
+        public List<Book> GetBooks()
+        {
+            return Books;
+        }
+
+        // Get a single book by ID
+        public Book GetBookById(int id)
+        {
+            return Books.FirstOrDefault(b => b.Id == id);
+        }
+
+        // Add a new book
+        public void AddBook(Book book)
+        {
+            book.Id = Books.Count + 1; // Simple auto-increment for ID
+            Books.Add(book);
+        }
+
+        // Update an existing book
+        public bool UpdateBook(int id, Book updatedBook)
+        {
+            var book = GetBookById(id);
+            if (book == null)
+                return false;
+
+            book.Title = updatedBook.Title;
+            book.Author = updatedBook.Author;
+            book.Year = updatedBook.Year;
+            book.Genre = updatedBook.Genre;
+            return true;
+        }
+
+        // Delete a book
+        public bool DeleteBook(int id)
+        {
+            var book = GetBookById(id);
+            if (book == null)
+                return false;
+
+            Books.Remove(book);
+            return true;
+        }
+    }
+}
---
+++ BooksController.cs
@@ -0,0 +1,67 @@
+﻿using LibraryAPI.Models;
+using LibraryAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BooksController()
+        {
+            _bookService = new BookService();
+        }
+
+        // GET: api/books
+        [HttpGet]
+        public ActionResult<List<Book>> GetAllBooks()
+        {
+            return _bookService.GetBooks();
+        }
+
+        // GET: api/books/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBookById(int id)
+        {
+            var book = _bookService.GetBookById(id);
+            if (book == null)
+                return NotFound();
+
+            return book;
+        }
+
+        // POST: api/books
+        [HttpPost]
+        public ActionResult AddBook([FromBody] Book newBook)
+        {
+            _bookService.AddBook(newBook);
+            return CreatedAtAction(nameof(GetBookById), new { id = newBook.Id }, newBook);
+        }
+
+        // PUT: api/books/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var result = _bookService.UpdateBook(id, updatedBook);
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+
+        // DELETE: api/books/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteBook(int id)
+        {
+            var result = _bookService.DeleteBook(id);
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
